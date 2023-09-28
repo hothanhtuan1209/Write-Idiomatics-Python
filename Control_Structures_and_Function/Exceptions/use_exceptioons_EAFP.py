ef1a7ee6378bc40ea19029733da360836c65ea48
Tuan <hothanhtuan1209@gmail.com>
2023-09-28 14:13:02 +0700

    Fix format code for use_exceptioons_EAFP.py file

--- Control_Structures_and_Function/Exceptions/use_exceptioons_EAFP.py
+++ Control_Structures_and_Function/Exceptions/use_exceptioons_EAFP.py
@@ -6,25 +6,26 @@
 
 # You shouldn't do like this
 def get_log_level(config_dict):
-    if 'ENABLE_LOGGING' in config_dict:
-        if config_dict['ENABLE_LOGGING'] != True:
+    if "ENABLE_LOGGING" in config_dict:
+        if config_dict["ENABLE_LOGGING"] != True:
             return None
-        elif not 'DEFAULT_LOG_LEVEL' in config_dict:
+        elif not "DEFAULT_LOG_LEVEL" in config_dict:
             return None
         else:
-            return config_dict['DEFAULT_LOG_LEVEL']
+            return config_dict["DEFAULT_LOG_LEVEL"]
 
     else:
         return None
 
+
 # You should do like this
 def get_log_level(config_dict):
     try:
-        if config_dict['ENABLE_LOGGING']:
-            return config_dict['DEFAULT_LOG_LEVEL']
+        if config_dict["ENABLE_LOGGING"]:
+            return config_dict["DEFAULT_LOG_LEVEL"]
 
     except KeyError:
-    # if either value wasn't present, a
-    # KeyError will be raised, so
-    # return None
+        # if either value wasn't present, a
+        # KeyError will be raised, so
+        # return None
         return None
