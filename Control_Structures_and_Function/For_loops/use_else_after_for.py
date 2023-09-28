8a1dbbf2f825c9bbea248ccb618cb605b9bf9d8f
Tuan <hothanhtuan1209@gmail.com>
2023-09-28 13:54:04 +0700

    Use black to update format code use_else_after_for.py

--- Control_Structures_and_Function/For_loops/use_else_after_for.py
+++ Control_Structures_and_Function/For_loops/use_else_after_for.py
@@ -6,25 +6,26 @@
 # You shouldn't do like this
 for user in get_all_users():
     has_malformed_email_address = False
-    print('Checking {}'.format(user))
-    
+    print("Checking {}".format(user))
+
     for email_address in user.get_all_email_addresses():
         if email_is_malformed(email_address):
             has_malformed_email_address = True
-            print('Has a malformed email address!')
+            print("Has a malformed email address!")
             break
 
 if not has_malformed_email_address:
-    print('All email addresses are valid!')
+    print("All email addresses are valid!")
+
 
 # You should do like this
 for user in get_all_users():
-    print('Checking {}'.format(user))
+    print("Checking {}".format(user))
 
 for email_address in user.get_all_email_addresses():
     if email_is_malformed(email_address):
-        print('Has a malformed email address!')
+        print("Has a malformed email address!")
         break
 
-else: # this else matches the `for` loop, not the if!
-    print('All email addresses are valid!')
+else:  # this else matches the `for` loop, not the if!
+    print("All email addresses are valid!")
