bc391388c1a5c75c854f1a68ad79731208dc0e0e
Tuan <hothanhtuan1209@gmail.com>
2023-10-02 09:00:37 +0700

    Add file for folder tuples

---
+++ Woking_with_data/Tuples/use_(_)_as_placeholder.py
@@ -0,0 +1,20 @@
+"""
+- Use _ as a placeholder for data in a tuple that should be ignored.
+- When setting a tuple equal to some ordered data, oftentimes not all of the data is actually needed.
+- Instead of creating throwaway variables with confusing names, use the _ as a placeholder to tell the reader, “This data will be discarded.”
+"""
+
+# Harmful solution
+(name, age, temp, temp2) = get_user_info(user)
+
+if age > 21:
+    output = '{name} can drink!'.format(name=name)
+# "Wait, where are temp and temp2 being used?"
+
+
+# Idiomatic solution
+(name, age, _, _) = get_user_info(user)
+
+if age > 21:
+    output = '{name} can drink!'.format(name=name)
+# "Clearly, only name and age are interesting"
