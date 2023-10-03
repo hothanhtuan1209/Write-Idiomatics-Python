b892223f0b3d341ed2f295652b8a98dc998340f5
Tuan <hothanhtuan1209@gmail.com>
2023-10-03 17:27:08 +0700

    Use black to fix format code in use_star_to_represent.py file

--- Woking_with_data/Lists/use_star_to_represent.py
+++ Woking_with_data/Lists/use_star_to_represent.py
@@ -15,9 +15,12 @@
 
 # Idiomatic solution
 some_list = ["a", "b", "c", "d", "e"]
+
 (first, second, *rest) = some_list
 print(rest)
+
 (first, *middle, last) = some_list
 print(middle)
+
 (*head, penultimate, last) = some_list
 print(head)
