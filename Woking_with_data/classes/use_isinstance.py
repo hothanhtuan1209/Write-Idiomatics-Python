7b6b6c86bb5d0af87cb8b3d4798511c50a415612
Tuan <hothanhtuan1209@gmail.com>
2023-10-03 15:54:34 +0700

    Add folder classes in chpater 2

---
+++ Woking_with_data/classes/use_isinstance.py
@@ -0,0 +1,49 @@
+"""
+- Use the isinstance function to determine the type of an object.
+- The isinstance function checks if an object matches a specified type or types, returning True if there's a match.
+- It's typically used for built-in types but can work with user-defined types as well.
+"""
+
+# Harmful solution
+# def get_size(some_object):
+#     """
+#     Return the "size" of *some_object*, where size = len(some_object) for
+#     sequences, size = some_object for integers and floats, and size = 1 for
+#     True, False, or None.
+#     """
+
+#     try:
+#         return len(some_object)
+    
+#     except TypeError:
+#         if some_object in (True, False, type(None)):
+#             return 1
+        
+#         else:
+#             return int(some_object)
+
+# print(get_size('hello'))
+# print(get_size([1, 2, 3, 4, 5]))
+# print(get_size(10.0))
+
+
+# Idiomatic solution
+def get_size(some_object):
+    """
+    Return the "size" of *some_object*, where size = len(some_object) for
+    sequences, size = some_object for integers and floats, and size = 1 for
+    True, False, or None.
+    """
+        
+    if isinstance(some_object, (list, dict, str, tuple)):
+        return len(some_object)
+    
+    elif isinstance(some_object, (bool, type(None))):
+        return 1
+    
+    elif isinstance(some_object, (int, float)):
+        return int(some_object)
+
+print(get_size('hello'))
+print(get_size([1, 2, 3, 4, 5]))
+print(get_size(10.0))
---
+++ Woking_with_data/classes/use_underscores.py
@@ -0,0 +1,100 @@
+"""
+- Use leading underscores in function and variable names to denote “private” data.
+- In Python, all attributes of a class, including data and methods, are inherently "public" by default, which means they can be accessed from outside the class.
+- To ensure privacy and prevent naming conflicts, Python follows naming conventions:
+    1. To mark attributes as "protected" (not intended for direct external access), a single underscore prefix (_) is used.
+    2. To mark attributes as "private" (not intended for direct access or subclass override), a double underscore prefix (__) is used.
+"""
+
+# Harmful solution
+# class Foo():
+#     def __init__(self):
+#         self.id = 8
+#         self.value = self.get_value()
+    
+#     def get_value(self):
+#         pass
+    
+#     def should_destroy_earth(self):
+#         return self.id == 42
+
+
+# class Baz(Foo):
+#     def get_value(self, some_new_parameter):
+#         """Since 'get_value' is called from the base class's
+#         __init__ method and the base class definition doesn't
+#         take a parameter, trying to create a Baz instance will
+#         fail.
+#         """
+        
+#         pass
+
+
+# class Qux(Foo):
+#     """We aren't aware of Foo's internals, and we innocently
+#     create an instance attribute named 'id' and set it to 42.
+#     This overwrites Foo's id attribute and we inadvertently
+#     blow up the earth.
+#     """
+    
+#     def __init__(self):
+#         super(Qux, self).__init__()
+#         self.id = 42
+    
+#     # No relation to Foo's id, purely coincidental
+
+# qux = Qux()
+# baz = Baz() # Raises 'TypeError'
+# qux.should_destroy_earth() # returns True
+# qux.id == 42 # returns True
+
+
+# Idiomatic solution
+class Foo():
+    def __init__(self):
+        """Since 'id' is of vital importance to us, we don't
+        want a derived class accidentally overwriting it. We'll
+        prepend with double underscores to introduce name
+        mangling.
+        """
+
+        self.__id = 8
+        self.value = self.__get_value() # Our 'private copy'
+    
+    def get_value(self):
+        pass
+    
+    def should_destroy_earth(self):
+        return self.__id == 42
+    
+    # Here, we're storing a 'private copy' of get_value,
+    # and assigning it to '__get_value'. Even if a derived
+    # class overrides get_value in a way incompatible with
+    # ours, we're fine
+    __get_value = get_value
+
+
+class Baz(Foo):
+    def get_value(self, some_new_parameter):
+        pass
+
+
+class Qux(Foo):
+    def __init__(self):
+        """Now when we set 'id' to 42, it's not the same 'id'
+        that 'should_destroy_earth' is concerned with. In fact,
+        if you inspect a Qux object, you'll find it doesn't
+        have an __id attribute. So we can't mistakenly change
+        Foo's __id attribute even if we wanted to.
+        """
+
+        self.id = 42
+        # No relation to Foo's id, purely coincidental
+        super(Qux, self).__init__()
+
+qux = Qux()
+baz = Baz() # Works fine now
+qux.should_destroy_earth() # returns False
+qux.id == 42 # returns True
+with pytest.raises(AttributeError):
+    getattr(qux, '__id')
