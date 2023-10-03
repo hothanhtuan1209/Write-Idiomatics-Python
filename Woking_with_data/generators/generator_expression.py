3ff23768db13af015b5cac368b758fbb7fa4fd88
Tuan <hothanhtuan1209@gmail.com>
2023-10-03 17:13:42 +0700

    Add folder generators in chapter 2

---
+++ Woking_with_data/generators/generator_expression.py
@@ -0,0 +1,14 @@
+"""
+- Prefer a generator expression to a list comprehension for simple iteration.
+- List comprehension: It creates a list and immediately fills all the elements of that list. This can take up a lot of memory if you have a large list.
+- Generator expression: It creates a generator, and instead of creating the complete list from scratch, it just generates each element of the list "on demand" as you iterate over it. This saves memory and allows you to work with large lists or even infinite strings without resource issues.
+"""
+
+# Harmful solution
+# for uppercase_name in [name.upper() for name in get_all_usernames()]:
+#     process_normalized_username(uppercase_name)
+
+
+# Idiomatic solution
+for uppercase_name in (name.upper() for name in get_all_usernames()):
+    process_normalized_username(uppercase_name)
---
+++ Woking_with_data/generators/use_generator_lazily.py
@@ -0,0 +1,65 @@
+"""
+- Use a generator to lazily load infinite sequences.
+- Generators are valuable in scenarios with infinite sequences or costly calculations, as they provide a way to iterate without precomputing.
+- They're special coroutines that yield iterables and save their state for efficient continuation.
+"""
+
+# Harmful solution
+# def get_twitter_stream_for_keyword(keyword):
+#     """Get's the 'live stream', but only at the moment
+#     the function is initially called. To get more entries,
+#     the client code needs to keep calling
+#     'get_twitter_livestream_for_user'. Not ideal.
+#     """
+    
+#     imaginary_twitter_api = ImaginaryTwitterAPI()
+#     if imaginary_twitter_api.can_get_stream_data(keyword):
+#         return imaginary_twitter_api.get_stream(keyword)
+
+# current_stream = get_twitter_stream_for_keyword('#jeffknupp')
+# for tweet in current_stream:
+#     process_tweet(tweet)
+
+
+# Uh, I want to keep showing tweets until the program is quit.
+# What do I do now? Just keep calling
+# get_twitter_stream_for_keyword? That seems stupid.
+
+# def get_list_of_incredibly_complex_calculation_results(data):
+#     return [first_incredibly_long_calculation(data),
+#             second_incredibly_long_calculation(data),
+#             third_incredibly_long_calculation(data),
+#             ]
+
+
+# Idiomatic solution
+def get_twitter_stream_for_keyword(keyword):
+    """Now, 'get_twitter_stream_for_keyword' is a generator
+    and will continue to generate Iterable pieces of data
+    one at a time until 'can_get_stream_data(user)' is
+    False (which may be never).
+    """
+    imaginary_twitter_api = ImaginaryTwitterAPI()
+    while imaginary_twitter_api.can_get_stream_data(keyword):
+        yield imaginary_twitter_api.get_stream(keyword)
+
+
+# Because it's a generator, I can sit in this loop until
+# the client wants to break out
+
+for tweet in get_twitter_stream_for_keyword('#jeffknupp'):
+    if got_stop_signal:
+        break
+
+process_tweet(tweet)
+
+def get_list_of_incredibly_complex_calculation_results(data):
+    """A simple example to be sure, but now when the client
+    code iterates over the call to
+    'get_list_of_incredibly_complex_calculation_results',
+    we only do as much work as necessary to generate the
+    current item.
+    """
+    yield first_incredibly_long_calculation(data)
+    yield second_incredibly_long_calculation(data)
+    yield third_incredibly_long_calculation(data)
