1a40c67b3eb568ff672e5ec7e90586841dcf7705
Tuan <hothanhtuan1209@gmail.com>
2023-10-04 11:01:06 +0700

    Update logic and format code in use_properties.py file to run, use flake8 and Pylint

--- woking_with_data/classes/use_properties.py
+++ woking_with_data/classes/use_properties.py
@@ -1,31 +1,38 @@
 """
 - Use properties to “future-proof” your class implementation.
-- Use properties in classes to prepare for potential changes in attribute handling.
-- This allows you to add logic or calculations later without altering existing code.
+- Use properties in classes to prepare for potential changes in attribute
+handling.
+- This allows you to add logic or calculations later without altering existing
+code.
 """
 
+
 # Harmful solution
-class Product():
-    def __init__(self, name, price):
-        self.name = name
-        # We could try to apply the tax rate here, but the object's price
-        # may be modified later, which erases the tax
-        self.price = price
+# class Product():
+#     def __init__(self, name, price):
+#         self.name = name  # We could try to apply the tax rate here,
+#         self.price = price  # may be modified later, which erases the tax
 
 
 # Idiomatic solution
 class Product():
+    TAX_RATE = 0.10
+
     def __init__(self, name, price):
         self.name = name
         self._price = price
-        
-        @property
-        def price(self):
+
+    @property
+    def price(self):
         # now if we need to change how price is calculated, we can do it
         # here (or in the "setter" and __init__)
-            return self._price * TAX_RATE
-        
-        @price.setter
-        def price(self, value):
+        return (self._price * self.TAX_RATE) + self._price
+
+    @price.setter
+    def price(self, value):
         # The "setter" function must have the same name as the property
-            self._price = value
+        self._price = value
+
+
+product = Product("lemon", 100.0)
+print(product.price)
