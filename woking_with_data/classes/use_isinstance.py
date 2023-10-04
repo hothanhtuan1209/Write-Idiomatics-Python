3b851b26bb4bb2eb3f6f9d8d8fd967bf3b01b56a
Tuan <hothanhtuan1209@gmail.com>
2023-10-04 11:06:30 +0700

    Update logic and format use_isinstance.py file, use flake8 and Pylint

--- woking_with_data/classes/use_isinstance.py
+++ woking_with_data/classes/use_isinstance.py
@@ -1,7 +1,9 @@
 """
 - Use the isinstance function to determine the type of an object.
-- The isinstance function checks if an object matches a specified type or types, returning True if there's a match.
-- It's typically used for built-in types but can work with user-defined types as well.
+- The isinstance function checks if an object matches a specified type or
+types, returning True if there's a match.
+- It's typically used for built-in types but can work with user-defined types
+as well.
 """
 
 # Harmful solution
@@ -14,11 +16,11 @@
 
 #     try:
 #         return len(some_object)
-    
+
 #     except TypeError:
 #         if some_object in (True, False, type(None)):
 #             return 1
-        
+
 #         else:
 #             return int(some_object)
 
@@ -34,15 +36,16 @@ def get_size(some_object):
     sequences, size = some_object for integers and floats, and size = 1 for
     True, False, or None.
     """
-        
+
     if isinstance(some_object, (list, dict, str, tuple)):
         return len(some_object)
-    
+
     elif isinstance(some_object, (bool, type(None))):
         return 1
-    
+
     elif isinstance(some_object, (int, float)):
         return int(some_object)
+
 
 print(get_size('hello'))
 print(get_size([1, 2, 3, 4, 5]))
