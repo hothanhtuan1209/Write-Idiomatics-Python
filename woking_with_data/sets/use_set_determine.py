747c5a487172359e46353b009bd0c862d0f53435
Tuan <hothanhtuan1209@gmail.com>
2023-10-04 15:12:17 +0700

    Update logic and format folder sets, use flake8 and Pylint, rename use_set_comprehension

--- woking_with_data/sets/use_set_determine.py
+++ woking_with_data/sets/use_set_determine.py
@@ -1,7 +1,9 @@
 """
 - Use sets to determine if two lists share any common values.
 - Checking if any elements from one list appear in another.
-- It simplifies the code by converting both lists to sets and finding common elements using the & operator, making the task easier and more efficient than using loops or other methods.
+- It simplifies the code by converting both lists to sets and finding common
+elements using the & operator, making the task easier and more efficient than
+using loops or other methods.
 """
 
 # Harmful solution
@@ -10,7 +12,7 @@
 
 # def has_duplicate(list_one, list_two):
 #     duplicate_name = False
-    
+
 #     for name in list_one:
 #         if name in list_two:
 #             duplicate_name = True
@@ -22,6 +24,7 @@
 list_one = ['Manny', 'Moe', 'Jack']
 list_two = ['Larry', 'Moe', 'Curly']
 
+
 def has_duplicate(list_one, list_two):
     """
     Check for duplicate elements between two lists.
@@ -31,7 +34,11 @@ def has_duplicate(list_one, list_two):
         list_two (list): The second list to check for duplicates.
 
     Returns:
-        set: A set containing elements that are duplicates between the two lists.
+        set: A set containing elements that are duplicates between the two
+        lists.
     """
 
     return set(list_one) & set(list_two)
+
+
+print(has_duplicate(list_one, list_two))
--- woking_with_data/sets/use_set_eliminate_duplicate.py
+++ woking_with_data/sets/use_set_eliminate_duplicate.py
@@ -1,46 +1,55 @@
 """
 - Use sets to eliminate duplicate entries from Iterable containers.
-- Sets are a useful tool for removing duplicate values from lists or dictionaries, thanks to their unique and efficient characteristics.
-- They can often be seamlessly integrated into existing code, making them a convenient choice for data manipulation.
+- Sets are a useful tool for removing duplicate values from lists or
+dictionaries, thanks to their unique and efficient characteristics.
+- They can often be seamlessly integrated into existing code, making them
+a convenient choice for data manipulation.
 """
 
 # Harmful solution
-unique_surnames = []
+# unique_surnames = []
 
-for surname in employee_surnames:
-    if surname not in unique_surnames:
-        unique_surnames.append(surname)
+# for surname in employee_surnames:
+#     if surname not in unique_surnames:
+#         unique_surnames.append(surname)
 
-def display(elements, output_format='html'):
-    if output_format == 'std_out':
-        for element in elements:
-            print(element)
 
-    elif output_format == 'html':
-        as_html = '<ul>'
-        for element in elements:
-            as_html += '<li>{}</li>'.format(element)
+# def display(elements, output_format='html'):
+#     if output_format == 'std_out':
+#         for element in elements:
+#             print(element)
 
-        return as_html + '</ul>'
+#     elif output_format == 'html':
+#         as_html = '<ul>'
+#         for element in elements:
+#             as_html += '<li>{}</li>'.format(element)
 
-    else:
-        raise RuntimeError('Unknown format {}'.format(output_format))
+#         return as_html + '</ul>'
+
+#     else:
+#         raise RuntimeError('Unknown format {}'.format(output_format))
 
 
 # Idiomatic solution
+employee_surnames = []
 unique_surnames = set(employee_surnames)
+
 
 def display(elements, output_format='html'):
     """
-    Create a set of unique values from an iterable and display them in the specified format.
+    Create a set of unique values from an iterable and display them in the
+    specified format.
 
     Args:
         elements (iterable): The source of elements.
-        output_format (str, optional): The format for displaying elements ('html' or 'std_out'). Default is 'html'.
+        output_format (str, optional): The format for displaying elements
+        ('html' or 'std_out'). Default is 'html'.
 
     Returns:
-        str or None: If 'output_format' is 'html', returns an HTML-formatted string; if 'output_format' is 'std_out', prints
-        elements to the standard output; otherwise, raises a RuntimeError for an unknown format.
+        str or None: If 'output_format' is 'html', returns an HTML-formatted
+        string; if 'output_format' is 'std_out', prints elements to the
+        standard output; otherwise, raises a RuntimeError for an unknown
+        format.
     """
 
     if output_format == 'std_out':
@@ -49,7 +58,7 @@ def display(elements, output_format='html'):
 
     elif output_format == 'html':
         as_html = '<ul>'
-        
+
         for element in elements:
             as_html += '<li>{}</li>'.format(element)
 
@@ -57,4 +66,3 @@ def display(elements, output_format='html'):
 
     else:
         raise RuntimeError('Unknown format {}'.format(output_format))
-
