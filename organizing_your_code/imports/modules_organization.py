71d6986485510a8074bd5d25def3ccf97b04f23f
Tuan <hothanhtuan1209@gmail.com>
2023-10-09 10:59:43 +0700

    Add file modules_organization.py in folder imports

---
+++ organizing_your_code/imports/modules_organization.py
@@ -0,0 +1,16 @@
+"""
+- Use the tuples to organize a long list of modules to import.
+- To manage long lists of module imports more effectively, consider using
+tuples to group multiple imports within a single import statement, making
+the code cleaner and more organized.
+"""
+
+
+# Harmful solution
+from django.db.models import AutoField, BigIntegerField, BooleanField, CharField
+from django.db.models import CommaSeparatedIntegerField, DateField, DateTimeField
+
+
+# Idiomatic solution
+from django.db.models import (AutoField, BigIntegerField, BooleanField,
+        CharField, CommaSeparatedIntegerField, DateField, DateTimeField)
