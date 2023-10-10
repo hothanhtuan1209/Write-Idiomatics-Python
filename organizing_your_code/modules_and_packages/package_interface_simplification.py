8e150ebcf5c11bedda729e662c8726aa0b02eb05
Tuan <hothanhtuan1209@gmail.com>
2023-10-10 11:03:48 +0700

    Update format code for package_interface_simplification.py file

--- organizing_your_code/modules_and_packages/package_interface_simplification.py
+++ organizing_your_code/modules_and_packages/package_interface_simplification.py
@@ -5,19 +5,16 @@ for external code.
 """
 
 
+# Idiomatic solution
+from gizmo.client.interface import Gizmo # __init__.py:
+from gizmo.client.contrib.utils import GizmoHelper # __init__.py:
+
+from gizmo import Gizmo, GizmoHelper #client code:
+
+
 # Harmful solution
 # If the gizmo directory has an emtpy __init__.py,
 # imports like the ones below are necessary, even
 # if Gizmo and GizmoHelper are all that clients should ever need to use
 # from gizmo.client.interface import Gizmo
 # from gizmo.client.contrib.utils import GizmoHelper
-
-
-# Idiomatic solution
-# __init__.py:
-
-from gizmo.client.interface import Gizmo
-from gizmo.client.contrib.utils import GizmoHelper
-
-#client code:
-from gizmo import Gizmo, GizmoHelper
