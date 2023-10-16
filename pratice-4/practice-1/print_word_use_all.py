ab5feceb74b6d6184d2dec8de779d73a66d21fc9
Tuan <hothanhtuan1209@gmail.com>
2023-10-16 16:15:00 +0700

    Update empty line after return command and use black to fix format code

--- pratice-4/practice-1/print_word_use_all.py
+++ pratice-4/practice-1/print_word_use_all.py
@@ -32,20 +32,21 @@ def count_words_vowel(required_letters):
     Count the number of words in a list that contain all the required letters.
 
     Parameters:
-        - required_letters (str): A string containing the letters that must be 
+        - required_letters (str): A string containing the letters that must be
         present in each word.
 
     Returns:
         - int: The count of words that contain all the required letters.
     """
 
-    words = input('Enter a list of words: ').split()
+    words = input("Enter a list of words: ").split()
     count = 0
 
     for word in words:
         if uses_all(word, required_letters):
             count += 1
+
     return count
 
 
-print(count_words_vowel('aeiou'))
+print(count_words_vowel("aeiou"))
